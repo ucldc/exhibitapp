499b77a1e894552da627876379ad628f44d0f629
amywieliczka <amywieliczka@gmail.com>
2020-03-20 13:42:28 -0700

    modified md5s3stash for exhibitapp; using boto3

--- exhibits/models.py
+++ exhibits/models.py
@@ -219,11 +219,7 @@ class Exhibit(models.Model):
                 url = settings.MEDIA_ROOT + "/" + name
                 if os.path.isfile(url):
                     field_instance = getattr(self, s3field)
-                    if settings.S3_STASH_ACL:
-                        acl=settings.S3_STASH_ACL
-                    else:
-                        acl=None
-                    report = md5s3stash("file://" + url, settings.S3_STASH, acl=acl)
+                    report = md5s3stash("file://" + url, settings.S3_STASH)
                     field_instance.storage.delete(name)
                     field_instance.name = report.md5
                     upload_to = self._meta.get_field(s3field).upload_to
@@ -294,11 +290,7 @@ class HistoricalEssay(models.Model):
                 url = settings.MEDIA_ROOT + "/" + name
                 if os.path.isfile(url):
                     field_instance = getattr(self, s3field)
-                    if settings.S3_STASH_ACL:
-                        acl=settings.S3_STASH_ACL
-                    else:
-                        acl=None
-                    report = md5s3stash("file://" + url, settings.S3_STASH, acl=acl)
+                    report = md5s3stash("file://" + url, settings.S3_STASH)
                     field_instance.storage.delete(name)
                     field_instance.name = report.md5
                     upload_to = self._meta.get_field(s3field).upload_to
@@ -418,11 +410,7 @@ class LessonPlan(models.Model):
                 url = settings.MEDIA_ROOT + "/" + name
                 if os.path.isfile(url):
                     field_instance = getattr(self, s3field)
-                    if settings.S3_STASH_ACL:
-                        acl=settings.S3_STASH_ACL
-                    else:
-                        acl=None
-                    report = md5s3stash("file://" + url, settings.S3_STASH, acl=acl)
+                    report = md5s3stash("file://" + url, settings.S3_STASH)
                     field_instance.storage.delete(name)
                     field_instance.name = report.md5
                     upload_to = self._meta.get_field(s3field).upload_to
@@ -519,11 +507,7 @@ class Theme(models.Model):
                 url = settings.MEDIA_ROOT + "/" + name
                 if os.path.isfile(url):
                     field_instance = getattr(self, s3field)
-                    if settings.S3_STASH_ACL:
-                        acl=settings.S3_STASH_ACL
-                    else:
-                        acl=None
-                    report = md5s3stash("file://" + url, settings.S3_STASH, acl=acl)
+                    report = md5s3stash("file://" + url, settings.S3_STASH)
                     field_instance.storage.delete(name)
                     field_instance.name = report.md5
                     upload_to = self._meta.get_field(s3field).upload_to
@@ -626,11 +610,7 @@ class ExhibitItem(models.Model):
                 url = settings.MEDIA_ROOT + "/" + name
                 if os.path.isfile(url):
                     field_instance = getattr(self, s3field)
-                    if settings.S3_STASH_ACL:
-                        acl=settings.S3_STASH_ACL
-                    else:
-                        acl=None
-                    report = md5s3stash("file://" + url, settings.S3_STASH, acl=acl)
+                    report = md5s3stash("file://" + url, settings.S3_STASH)
                     field_instance.storage.delete(name)
                     field_instance.name = report.md5
                     upload_to = self._meta.get_field(s3field).upload_to
