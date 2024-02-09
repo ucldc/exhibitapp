0da7f3191eae62b50c406fbcf83bca652ea0feb9
amy wieliczka <amywieliczka@gmail.com>
2024-02-09 12:26:06 -0800

    Update markdownify template_tag per markdown changelog

--- exhibits/templatetags/markdown_filter.py
+++ exhibits/templatetags/markdown_filter.py
@@ -6,10 +6,14 @@ from markdown.extensions import Extension
 # per notes in the release docs for version 2.5, used an EscapeHTML 
 # extension to replace safe_mode="escape" (deprecated)
 
+# https://python-markdown.github.io/changelog/#safe_mode-and-html_replacement_text-keywords-deprecated
+# https://python-markdown.github.io/changelog/#md_globals-keyword-deprecated-from-extension-api
+# https://python-markdown.github.io/changelog/#previously-deprecated-objects-have-been-removed
+
 class EscapeHtml(Extension):
-	def extendMarkdown(self, md, md_globals):
-		del md.preprocessors['html_block']
-		del md.inlinePatterns['html']
+	def extendMarkdown(self, md):
+		md.preprocessors.deregister('html_block')
+		md.inlinePatterns.deregister('html')
 
 register = Library()
 
